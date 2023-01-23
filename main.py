fc71dd76f84d7f906b12735deda9ef65b777e505
BigEd12 <117647968+BigEd12@users.noreply.github.com>
2023-01-23 10:18:44 +0100

    Further upload
    Revision of main.py to include decrypt function, add artwork, and improve UX

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 from dictionary import morse_dict
+from art import logo
 
-def convert():
-    string = input("Welcome to the Text-to-Morse-Code. Please enter the message you'd like to convert to Morse Code:\n").lower()
-    input_as_list = [*string]
-    try:
-        output = [morse_dict[letter] for letter in input_as_list]
-    except KeyError:
-        print("You've entered an unknown character.")
 
-    else:
-        str_join = " ".join(output)
-        print("Your message in Morse:", str_join)
+def encrypt(word):
+    output = ""
+    for letter in word:
+        if letter == " ":
+            output += " | "
+        else:
+            output += morse_dict[letter] + " "
+    return output
 
-convert()
+
+def decrypt(word):
+    split_word = word.split()
+    output = ""
+    for i in range(len(split_word)):
+        if split_word[i] == "|":
+            output += " "
+        else:
+            for key, value in morse_dict.items():
+                if split_word[i] == value:
+                    output += key
+    return output
+
+print(logo)
+print("Welcome to the Morse Code converter.\n I can convert text to Morse Code, or vice versa.")
+if input("Do you want to encrypt or decrypt? Type 'e' or 'd':\n") == "e":
+    word = input("What would you like to encrypt?\n").lower()
+    print(f"Your word {word} has been encrypted to: {encrypt(word)}")
+else:
+    word = input("What would you like to decrypt?\n")
+    print(f"Your Morse code ({word}) has been decrypted as: {decrypt(word)} ")
+
+
+
+
